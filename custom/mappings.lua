81f9e566f1eb6088a581f9541461a050b6325b15
joakes <joshuapoakes@outlook.com>
2023-07-02 14:44:51 -0700

    override LSP Hover on K

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -1,13 +1,19 @@
 ---@type MappingsTable
 local M = {}
 
+M.disabled = {
+	n = {
+		["K"] = "", -- Disabling LSP Hover defined in core/mappings.lua
+	},
+}
+
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<S-j>"] = { "5j", "jump 5 lines down", opts = { nowait = true } },
-    ["<S-k>"] = { "5k", "jump 5 lines up", opts = { nowait = true } },
-    ["<leader>j"] = { "J", "remove new line chars at end of line", opts = { nowait = true } },
-    },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		["J"] = { "5j", "jump 5 lines down", opts = { nowait = true } },
+		["K"] = { "5k", "jump 5 lines up", opts = { nowait = true } },
+		["<leader>j"] = { "J", "remove new line chars at end of line", opts = { nowait = true } },
+	},
 }
 
 -- more keybinds!
